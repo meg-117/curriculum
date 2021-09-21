579ae732d6971e7e37bc25d0dfb7b8219e92ef35
meg-117 <90508215+meg-117@users.noreply.github.com>
2021-09-21 15:03:06 +0900

    配列・連想配列の課題
    配列・連想配列の課題作成

---
+++ 2-7/index.php
@@ -0,0 +1,19 @@
+<?php
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
+$colors = ["yellow" => "黄色"] 
+?>
+
+array(3) {
+    ["red"]=> string(3) "赤"
+    ["blue"]=> string(3) "青"
+    ["green"]=> string(3) "緑"
+}
+
+<br />
+
+array(4) {
+    ["red"]=> string(3) "赤"
+    ["blue"]=> string(3) "青"
+    ["green"]=> string(3) "緑"
+    ["yellow"]=> string(6) "黄色"
+}
