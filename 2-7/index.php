fde2d41ca8c7b4605c5a79cc9989bc1129ceadf0
meg-117 <90508215+meg-117@users.noreply.github.com>
2021-09-23 11:26:17 +0900

    配列・連想配列の課題修正
    配列・連想配列の課題修正

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,19 +1,9 @@
 <?php
 $colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
-$colors = ["yellow" => "黄色"] 
+var_dump($colors);
 ?>
-
-array(3) {
-    ["red"]=> string(3) "赤"
-    ["blue"]=> string(3) "青"
-    ["green"]=> string(3) "緑"
-}
-
 <br />
-
-array(4) {
-    ["red"]=> string(3) "赤"
-    ["blue"]=> string(3) "青"
-    ["green"]=> string(3) "緑"
-    ["yellow"]=> string(6) "黄色"
-}
+<?php
+$colors["yellow"]= "黄色";
+var_dump($colors);
+?>
