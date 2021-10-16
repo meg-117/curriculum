872a3f54478254e7736b924000963bc50ef49207
meg-117 <90508215+meg-117@users.noreply.github.com>
2021-10-16 21:04:58 +0900

    第２章チェックテスト課題_修正１
    課題修正_１

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -1,14 +1,24 @@
-<link rel="stylesheet" href="style.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
 
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $my_name = $_POST['my_name'];
-$number =$_POST['number'];
-$answer_number =$_POST['answer_number'];
-$language =$_POST['language'];
-$answer_language =$_POST['answer_language'];
-$command =$_POST['command'];
-$answer_command =$_POST['answer_command'];
+$number = $_POST['number'];
+$answer = $_POST['answer'];
+$language = $_POST['language'];
+$command = $_POST['command'];
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 
@@ -17,29 +27,26 @@ $answer_command =$_POST['answer_command'];
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($number == $answer_number){
-	echo "正解！";
-}else{
-	echo "不正解･･･";
+function check($check,$answer){
+	if($check == $answer){
+		echo "正解！";
+	}else{
+		echo "不正解･･･";
+	}
 }
+
+check ($number,$answer);
+
 ?>
 
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($language == $answer_language){
-	echo "正解！";
-}else{
-	echo "不正解･･･";
-}
+check ($language,$answer);
 ?>
 
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($command == $answer_command){
-	echo "正解！";
-}else{
-	echo "不正解･･･";
-}
+check ($command,$answer);
 ?>
--- 2-18/question.php
+++ 2-18/question.php
@@ -1,12 +1,22 @@
-<link rel="stylesheet" href="style.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
 
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
     $my_name = $_POST['my_name'];
 //①画像を参考に問題文の選択肢の配列を作成してください。
-    $number = ['80', '22', '20', '21'];
-    $language =['PHP','Python','JAVA','HTML'];
-    $command =['join','select','insert','update'];
+    $number = array('80', '22', '20', '21');
+    $language =array('PHP','Python','JAVA','HTML');
+    $command =array('join','select','insert','update');
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
 
 ?>  
@@ -19,7 +29,7 @@
 <form action="answer.php" method="post">
 <?php
 foreach ($number as $value) {?>
-<input type="radio" name="number" value="'{$value}'" style="color:white;">
+<input type="radio" name="number" value="<?php echo $value; ?>"/>
 <?php echo $value;
 }
 ?>
@@ -27,7 +37,7 @@ foreach ($number as $value) {?>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php
 foreach ($language as $value) {?>
-<input type="radio" name="language" value="'{$value}'" style="color:white;">
+<input type="radio" name="language" value="<?php echo $value; ?>"/>
 <?php echo $value;
 }
 ?>
@@ -35,7 +45,7 @@ foreach ($language as $value) {?>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php
 foreach ($command as $value) {?>
-<input type="radio" name="command" value="'{$value}'" style="color:white;">
+<input type="radio" name="command" value="<?php echo $value; ?>"/>
 <?php echo $value;
 }
 ?>
@@ -46,16 +56,20 @@ foreach ($command as $value) {?>
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
 <?php
 $answer_number = "20";
-$answer_language = "PHP";
+$answer_language = "HTML";
 $answer_command = "insert";
 ?>
 
-<input type="hidden" name="my_name" value="<?php echo $my_name; ?>">
-<input type="hidden" name="number" value ="<?php echo $number; ?>">
-<input type="hidden" name="answer_number" value ="<?php echo $answer_number; ?>">
-<input type="hidden" name="language" value ="<?php echo $language; ?>">
-<input type="hidden" name="answer_language" value ="<?php echo $answer_language; ?>">
-<input type="hidden" name="command" value ="<?php echo $command; ?>">
-<input type="hidden" name="answer_command" value ="<?php echo $answer_command; ?>">
+<input type="hidden" name="my_name" value="<?php echo $my_name; ?>"/>
+<input type="hidden" name="number" value ="<?php echo $value; ?>"/>
+<input type="hidden" name="answer" value ="<?php echo $answer_number; ?>"/>
+<input type="hidden" name="language" value ="<?php echo $value; ?>"/>
+<input type="hidden" name="answer" value ="<?php echo $answer_language; ?>"/>
+<input type="hidden" name="command" value ="<?php echo $value; ?>"/>
+<input type="hidden" name="answer" value ="<?php echo $answer_command; ?>"/>
 
 </form>
+
+</body>
+
+</html>
