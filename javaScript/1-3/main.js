e137757bf4bf930bd7b377c0456a78d3c4a1c3b7
meg-117 <90508215+meg-117@users.noreply.github.com>
2021-11-19 19:10:17 +0900

    JS 条件分岐・演算子 課題提出
    条件分岐・演算子課題

---
+++ javaScript/1-3/main.js
@@ -0,0 +1,23 @@
+let a = 8;
+if ( a != 10){
+    console.log("変数aは10ではない");
+}
+
+let b = 16;
+if (b >= 10 && b <= 20 && b/2) {
+    console.log("変数bは10以上20未満、かつ偶数である。");  
+}
+
+let x = 10;
+if ( x >= 10 && x <= 20){
+    console.log("成功です")
+} else {
+    console.log("失敗です")
+}
+
+let y = 0;
+if ( y % 2 === 0){
+    console.log("偶数です")
+}else if (y % 2 !==0){
+    console.log("奇数です")
+}
