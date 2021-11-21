66e66ec000b14d382aa54534071585596d40c975
meg-117 <90508215+meg-117@users.noreply.github.com>
2021-11-21 18:24:53 +0900

    JS 1章チェックテスト 課題
    1章チェックテスト　課題提出

---
+++ javaScript/Check1/main.js
@@ -0,0 +1,23 @@
+//問1
+
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+
+function isEven(numbers) {
+    return numbers % 2 ===0;
+}
+
+console.log(numbers.filter(isEven) + 'は偶数です');
+ 
+//問2
+
+function car (getNumGas){
+
+console.log();
+return getNumGas;
+}
+
+let gas = car ('〇〇');
+let num = car ('△△');
+
+console.log ('ガソリンは' + gas +'です。ナンバーは' + num + 'です');
